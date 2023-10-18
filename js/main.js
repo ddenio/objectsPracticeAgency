fb7b7a22de1ce35f5413139e47bbe61abfe6ac1c
Derin DeNio <derindenio@gmail.com>
2023-10-18 11:43:19 -0600

    Added a Back-end Dev Child Class, created 3 objects, created an agency using the 3 created objects, iterated through 3 objects to use same method on all 3 (polymorphism

--- js/main.js
+++ js/main.js
@@ -27,7 +27,33 @@ class Frontend extends Contractor{
         super(name,role,salary);
         this._language = language;
     }
+    get language() {
+        return this._language
+    }
     sayHello(){
         console.log(`Hello! I'm ${this._name}, i am a ${this._role} and use the ${this._language} programming language!`)
     }
+}
+
+class Backend extends Contractor{
+    constructor(name,role,salary,language){
+        super(name,role,salary);
+        this._language = language;
+    }
+    get language(){
+        return this._language
+    }
+    sayHello(){
+        console.log(`Hello! I'm ${this._name}, i am a ${this._role} and use the ${this._language} programming language!`)
+    }
+}
+
+let jim = new Contractor('Jim', 'Front-End', 85000);
+let peter = new Frontend('Peter','Front-End',90000,'JavaScript');
+let matt = new Backend('Matt','Back-end',150000,'C++')
+
+let agency = [jim,peter,matt]
+
+for(person of agency){
+    person.sayHello()
 }
